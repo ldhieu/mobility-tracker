3a48aa7d3f9026e6e66f7363fd3e320a1f19f8e5
Maham Khan <mfk306@nyu.edu>
2022-02-09 00:06:43 +0000

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -57,20 +57,20 @@ def government_response_reader():
 
 @st.cache(suppress_st_warning=True,show_spinner=False)
 def facebook_data_reader():
-    url20 = download_from_hdx()[0]
-    # url21 = download_from_hdx()[1]
-    print(url20)
-    # print(url21)
-    y20 = urlopen(url20)
-    # y21 = urlopen(url21)
-    zipfile20 = ZipFile(BytesIO(y20.read()))
-    # zipfile21 = ZipFile(BytesIO(y21.read()))
-    file20 = [i for i in zipfile20.namelist() if 'movement' in i][0]
-    # file21 = [i for i in zipfile21.namelist() if 'movement' in i][0]
-    df20 = pd.read_csv(zipfile20.open(file20),sep='\t')
-    # df21 = pd.read_csv(zipfile21.open(file21),sep='\t')
-    # df = pd.concat([df20,df21],ignore_index=True)
-    df = df20
+    # url20 = download_from_hdx()[0]
+    url21 = download_from_hdx()[1]
+    # print(url20)
+    print(url21)
+    # y20 = urlopen(url20)
+    y21 = urlopen(url21)
+    # zipfile20 = ZipFile(BytesIO(y20.read()))
+    zipfile21 = ZipFile(BytesIO(y21.read()))
+    # file20 = [i for i in zipfile20.namelist() if 'movement' in i][0]
+    file21 = [i for i in zipfile21.namelist() if 'movement' in i][0]
+    # df20 = pd.read_csv(zipfile20.open(file20),sep='\t')
+    df21 = pd.read_csv(zipfile21.open(file21),sep='\t')
+    df = pd.concat([df20,df21],ignore_index=True)
+    df = df21
     df = df[df['country'].isin(['VNM','TLS','PHL'])]
     df['ds'] = pd.to_datetime(df['ds'])
     df['Change in Mobility'] = (df['all_day_bing_tiles_visited_relative_change']*100).round(2)
